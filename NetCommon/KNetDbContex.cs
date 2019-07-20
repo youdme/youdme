87890d0ade007948e77e096143b5e3eba372600e
Ling xinggao <youdme@163.com>
2019-07-20 21:42:47 +0800

    添加项目文件。

---
+++ NetCommon/KNetDbContex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+
+namespace NetCommon
+{
+    public class KNetDbContext : DbContext
+    {
+        public KNetDbContext()
+        {
+
+        }
+
+        public KNetDbContext(DbContextOptions<KNetDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(NetCommon.Configuration.GetItem("ConnectionStrings"));
+            }
+        }
+
+
+        #region Entity DbSet<>
+        //public virtual DbSet<m_AppManager> m_AppManager { get; set; }
+        #endregion
+
+    }
+}
